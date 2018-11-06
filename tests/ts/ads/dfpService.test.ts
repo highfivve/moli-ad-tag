5259c1e95ef96c087bf0f52c416233e2485ffe01
Nepomuk Seiler <nepomuk.seiler@gutefrage.net>
2018-11-06 15:53:30 +0100

    Add mocks for googletag and pubads services

--- tests/ts/ads/dfpService.test.ts
+++ tests/ts/ads/dfpService.test.ts
@@ -3,12 +3,46 @@ import browserEnv = require('browser-env');
 browserEnv(['window', 'document']);
 
 import { expect } from 'chai';
+import * as Sinon from 'sinon';
 import { moli } from '../../../source/ts/ads/dfpService';
+import { googletag } from '../../../source/ts/types/googletag';
+
+
+const pubAdsServiceStub: googletag.IPubAdsService = {
+  set: (_key: string, _value: string): googletag.IPubAdsService => { return pubAdsServiceStub; },
+  setTargeting: (_key: string, _value: string | string[]): googletag.IPubAdsService => { return pubAdsServiceStub; },
+  setRequestNonPersonalizedAds: (_value: 0 | 1): googletag.IPubAdsService => { return pubAdsServiceStub; },
+  refresh: (slots?: googletag.IAdSlot[], options?: { changeCorrelator: boolean }): void => { return; },
+  enableSingleRequest: (): boolean => { return true; },
+  enableAsyncRendering: (): boolean => { return true; },
+  disableInitialLoad: (): void => { return; },
+  getSlots: (): googletag.IAdSlot[] => { return []; },
+  addEventListener: (_eventType: string, _listener: (event: any) => void): googletag.IPubAdsService => {
+    return pubAdsServiceStub;
+  }
+};
+
+const googletagStub: googletag.IGoogleTag = {
+  cmd: [],
+  defineSlot: (_adUnitPath: string, _size: googletag.Size[], _slotId: string): googletag.IAdSlot => { throw new Error('stub'); },
+  defineOutOfPageSlot: (_adUnitPath: string, _slotId: string): googletag.IAdSlot => { throw new Error('stub'); },
+  destroySlots: (_opt_slots: googletag.IAdSlot[]): void => { return; },
+  display: (_id: string): void => { return; },
+  enableServices: (): void => { return; },
+  pubads: (): googletag.IPubAdsService => pubAdsServiceStub
+};
 
 // tslint:disable: no-unused-expression
 describe('moli', () => {
+  const sandbox = Sinon.createSandbox();
+
+  afterEach(() => {
+    sandbox.reset();
+  });
+
   it('should have been exported', () => {
     expect(moli).to.be.ok;
   });
+
 });
 // tslint:enable
