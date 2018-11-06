a67062f0ed8822f35f5957456068057f1abf7371
Benjamin <benjamin.jentsch@gutefrage.net>
2018-11-06 08:51:03 +0100

    GD-1009, GD-1010: unit tests with mocha, chai, and sinon

---
+++ source/ts/ads/dfpService.test.ts
@@ -0,0 +1,12 @@
+import browserEnv = require('browser-env');
+
+browserEnv([ 'window', 'document' ]);
+
+import { expect } from 'chai';
+import { moli } from './dfpService';
+
+describe('moli', () => {
+  it('should have been exported', () => {
+    expect(moli).to.be.ok;
+  });
+});
