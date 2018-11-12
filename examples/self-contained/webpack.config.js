c2e46f595cfe0cd473305c9931f244b583000c8b
Nepomuk Seiler <nepomuk.seiler@gutefrage.net>
2018-11-12 17:22:46 +0100

    Add semicolon in webpack config

--- examples/self-contained/webpack.config.js
+++ examples/self-contained/webpack.config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
 module.exports = {
