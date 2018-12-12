974975f33ea4ef25de9754170db22710019c344e
Nepomuk Seiler <nepomuk.seiler@gutefrage.net>
2018-12-12 15:57:30 +0100

    Consistent webpack config for example projects

--- examples/instant/webpack.config.js
+++ examples/instant/webpack.config.js
@@ -4,47 +4,49 @@ const path = require('path');
 const fs = require('fs');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  devtool: process.env.NODE_ENV === 'production' ? 'none' : 'inline-source-map',
-  entry: './index.ts',
-  output: {
-    filename: 'instant_self_contained_[chunkHash].js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        loader: 'ts-loader',
-        options: { 'allowTsInNodeModules': true }
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.ts', '.js']
-  },
-  // local development
-  devServer: {
-    https: {
-      key: fs.readFileSync(path.join(__dirname, 'certs/selfsigned.key')),
-      cert: fs.readFileSync(path.join(__dirname, 'certs/selfsigned.crt')),
-      ca: fs.readFileSync(path.join(__dirname, 'certs/selfsigned.pem'))
+module.exports = (_, argv) => {
+  return {
+    devtool: argv.mode === 'production' ? 'none' : 'inline-source-map',
+    entry: './index.ts',
+    output: {
+      filename: 'instant_self_contained_[chunkHash].js'
     },
-    contentBase: [
-      path.join(__dirname, 'dist'),
-      // always use the latest moli-debugger
-      '../../moli-debugger/dist'
-    ],
-    compress: true,
-    port: 9000,
-    allowedHosts: [
-      'localhost',
-      '.gutefrage.net'
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          loader: 'ts-loader',
+          options: {'allowTsInNodeModules': true}
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.ts', '.js']
+    },
+    // local development
+    devServer: {
+      https: {
+        key: fs.readFileSync(path.join(__dirname, 'certs/selfsigned.key')),
+        cert: fs.readFileSync(path.join(__dirname, 'certs/selfsigned.crt')),
+        ca: fs.readFileSync(path.join(__dirname, 'certs/selfsigned.pem'))
+      },
+      contentBase: [
+        path.join(__dirname, 'dist'),
+        // always use the latest moli-debugger
+        '../../moli-debugger/dist'
+      ],
+      compress: true,
+      port: 9000,
+      allowedHosts: [
+        'localhost',
+        '.gutefrage.net'
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Instant / Eager Init - Publisher Demo Page',
+        template: 'demo/index.html'
+      })
     ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Instant / Eager Init - Publisher Demo Page',
-      template: 'demo/index.html'
-    })
-  ]
+  }
 };
