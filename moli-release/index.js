e151fa5f22f889d9526feaa079b3192666b734bb
Robin Mattis <robin.mattis@gutefrage.net>
2020-10-28 12:38:14 +0100

    GD-2346: Added moli-release cli script

---
+++ moli-release/index.js
@@ -0,0 +1,75 @@
+#!/usr/bin/env node
+'use strict'
+
+const readline = require('readline');
+const fs = require('fs');
+const { exec } = require('child_process');
+
+const releasesJson = require('./releases.json');
+const packageJson = require('./package.json');
+
+const packageJsonVersion = packageJson.version;
+const versions = releasesJson.versions ?? [];
+
+const majorVersionNumber = Number(packageJsonVersion.split('.')[0]);
+const version = (majorVersionNumber + 1) + '.0.0';
+
+packageJson.version = version;
+
+fs.writeFileSync('package.json', JSON.stringify(packageJson, null, 2));
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+enterChanges(rl, function (changelog) {
+  const change = {
+    'version': version,
+    'changelog': changelog
+  };
+  versions.push(change);
+
+  let releasesJsonContent = {
+    'currentVersion': version,
+    'versions': versions
+  };
+
+  fs.writeFileSync('releases.json', JSON.stringify(releasesJsonContent, null, 2));
+
+  console.log('Successfully created release ' + version);
+});
+
+rl.on('close', function () {
+  setTimeout(() => {
+    exec(`git add package.json releases.json && git commit -m 'v${version}' && git tag -a v${version} -m v${version} && git push && git push origin v${version}`, (err, stdout, stderr) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Successfully pushed new version');
+      }
+      process.exit(0);
+    });
+  }, 1000);
+});
+
+/**
+ * Recursive function that waits for user input to create a changelog.
+ * @param rl The readline object
+ * @param callback The callback which returns the changelog array.
+ * @param changelog The initial changelog. (works as accumulator)
+ */
+function enterChanges(rl, callback, changelog = []) {
+  rl.question('Please enter your changes: (leave empty for submission): ', function (change) {
+    //user leaves empty, so we can return
+    if (change == null || change === '') {
+      callback(changelog);
+      rl.close();
+      return;
+    }
+
+    // recursively call this function until the user leaves the input empty
+    changelog.push(change);
+    enterChanges(rl, callback, changelog);
+  });
+}
