a708ec995b064e866c95b29834d481d14ec63e84
Robin Mattis <robin.mattis@gutefrage.net>
2020-11-04 11:01:05 +0100

    GD-2346: moli-release script recode in typescript

---
+++ moli-release/webpack.config.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const path = require('path');
+
+module.exports = (env, argv) => ({
+  mode: 'development',
+  target: 'node',
+  devtool: argv.mode === 'production' ? 'none' : 'inline-source-map',
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'lib'),
+    filename: 'moli-release.js'
+  },
+  node: {
+    __dirname: false,
+    __filename: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        options: { allowTsInNodeModules: true }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.css']
+  }
+});
