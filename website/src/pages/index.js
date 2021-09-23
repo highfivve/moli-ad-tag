5cee9c045b2b25110fe21d82d0726e690114ae6f
Nepomuk Seiler <nepomuk.seiler@gutefrage.net>
2021-09-23 22:48:17 +0200

    Work on the documentation starting page

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -37,6 +37,49 @@ const features = [
   }
 ];
 
+const demandSources = [
+  {
+    title: 'Google',
+    description: (
+      <>
+        Google AdX and Open Bidding provide a strong foundation are come directly with your Google
+        Ad Manager
+      </>
+    ),
+    href: 'https://support.google.com/admanager/answer/6321605'
+  },
+  {
+    title: 'Prebid.js',
+    description: (
+      <>
+        The defacto standard for client side header bidding. Types for popular bidders are provided
+        to ensure easy configuration.
+      </>
+    ),
+    href: 'https://prebid.org/product-suite/prebid-js/'
+  },
+  {
+    title: 'Prebid Server',
+    description: (
+      <>
+        The open source server side header bidding implementation. Removes the burden from the
+        client side to do all the heavy lifting. This requires prebid server instance.
+      </>
+    ),
+    href: 'https://prebid.org/product-suite/prebid-server/'
+  },
+  {
+    title: 'Amazon TAM',
+    description: (
+      <>
+        The Amazon transparent ad marketplace. A server side header bidding solution like Google
+        Open Bidding or Prebid Server.
+      </>
+    ),
+    href: 'https://aps.amazon.com/aps/transparent-ad-marketplace/'
+  }
+];
+
 function Feature({ imageUrl, title, description }) {
   const imgUrl = useBaseUrl(imageUrl);
   return (
@@ -48,6 +91,24 @@ function Feature({ imageUrl, title, description }) {
       )}
       <h3>{title}</h3>
       <p>{description}</p>
+    </div>
+  );
+}
+
+function DemandSource({ title, description, href }) {
+  return (
+    <div className="col col--3">
+      <div className="card">
+        <div className="card__header">
+          <h3>{title}</h3>
+        </div>
+        <div className="card__body">{description}</div>
+        <div className="card__footer">
+          <a href={href} className="button button--outline button--primary button--block">
+            Read more
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
@@ -78,17 +139,40 @@ function Home() {
         </div>
       </header>
       <main>
-        {features && features.length > 0 && (
-          <section className={styles.features}>
-            <div className="container">
-              <div className="row">
-                {features.map((props, idx) => (
-                  <Feature key={idx} {...props} />
-                ))}
+        <section className={styles.features}>
+          <div className="container">
+            <div className="row">
+              {features.map((props, idx) => (
+                <Feature key={idx} {...props} />
+              ))}
+            </div>
+            <div className="row margin-top--lg">
+              <div class="col col--12 padding--md">
+                <h1>Is this for me?</h1>
+                <p>
+                  You are a <strong>publisher</strong> and want to take control of your
+                  monetization. Along with some IT folks this add tag requires
+                </p>
+                <ul>
+                  <li>Google Ad Manager as an ad server</li>
+                  <li>a TCF 2.0 compliant CMP</li>
+                </ul>
               </div>
             </div>
-          </section>
-        )}
+
+            <div class="row">
+              <div className="col col--12">
+                <h1>What is supported?</h1>
+              </div>
+            </div>
+
+            <div class="row">
+              {demandSources.map((props, idx) => (
+                <DemandSource key={idx} {...props} />
+              ))}
+            </div>
+          </div>
+        </section>
       </main>
     </Layout>
   );
