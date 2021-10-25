4a731fdece324c990c6873bdebbb99f0224a695d
Robin Mattis <robin.mattis@gutefrage.net>
2021-10-25 11:29:53 +0200

    GD-3821: Send adUnitPaths in yield optimization request

--- modules/yield-optimization/yieldOptimizationService.ts
+++ modules/yield-optimization/yieldOptimizationService.ts
@@ -62,10 +62,12 @@ export class YieldOptimizationService {
    *
    * @param device - the device label for the LabelConfigService
    * @param adUnitPathVariables - from the config targeting object
+   * @param adUnitPaths All adUnitPaths configured in the slot config.
    */
   public init(
     device: 'mobile' | 'desktop',
-    adUnitPathVariables: AdUnitPathVariables
+    adUnitPathVariables: AdUnitPathVariables,
+    adUnitPaths: string[]
   ): Promise<void> {
     // if a desktop label is present, the yield optimization service will request desktop price rules
     // otherwise mobile
@@ -91,9 +93,22 @@ export class YieldOptimizationService {
         break;
       case 'dynamic':
         this.isEnabled = true;
+
+        const excludedAdUnitPaths = this.yieldConfig.excludedAdUnitPaths;
+
+        // The list of adUnitPaths we want to get the priceRules for.
+        const filteredAdUnitPaths = adUnitPaths.filter(
+          adUnitPath => excludedAdUnitPaths.indexOf(adUnitPath) < 0
+        );
+
+        const resolvedAdUnits = filteredAdUnitPaths.map(adUnitPath =>
+          resolveAdUnitPath(adUnitPath, this.adUnitPathVariables)
+        );
+
         this.adUnitPricingRuleResponse = this.loadConfigWithRetry(
           this.yieldConfig.configEndpoint,
-          3
+          3,
+          resolvedAdUnits
         )
           .then(config => {
             this.log.info(
@@ -169,6 +184,7 @@ export class YieldOptimizationService {
   private loadConfigWithRetry(
     configEndpoint: string,
     retriesLeft: number,
+    adUnitPaths: string[],
     lastError: any | null = null
   ): Promise<AdunitPriceRulesResponse> {
     if (retriesLeft <= 0) {
@@ -187,7 +203,9 @@ export class YieldOptimizationService {
         body: JSON.stringify({
           device: this.device,
           // GD-2996 - temporary migration to new key
-          key: 'adUnitPath'
+          key: 'adUnitPath',
+          // GD-3821 - send adUnitPaths for alarm on misconfigured adUnits
+          adUnitPaths: adUnitPaths
         })
       })
       .then(response => {
