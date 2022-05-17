881806c3ec584abf8cb7ac6ffc332928907be4f7
Nepomuk Seiler <muuki88@users.noreply.github.com>
2022-05-17 20:45:24 +0200

    GD-4659 make labels generic which allows to narrow down the labels type (#44)

--- ad-tag/source/ts/types/moli.ts
+++ ad-tag/source/ts/types/moli.ts
@@ -887,15 +887,15 @@ export namespace Moli {
    * @see [Size Mapping](https://prebid.org/dev-docs/examples/size-mapping.html)
    * @see [requestBids with labels](https://prebid.org/dev-docs/publisher-api-reference.html#module_pbjs.requestBids)
    */
-  export interface SizeConfigEntry {
+  export interface SizeConfigEntry<Label = string> {
     /** media query that must match if the sizes are applicable */
     readonly mediaQuery: string;
 
     /** optional array of labels. All labels must be present if the sizes should be applied */
-    readonly labelAll?: string[];
+    readonly labelAll?: Label[];
 
     /** optional array of labels. All labels must **not** be present if the sizes should be applied */
-    readonly labelNone?: string[];
+    readonly labelNone?: Label[];
 
     /** static sizes that are support if the media query matches */
     readonly sizesSupported: DfpSlotSize[];
