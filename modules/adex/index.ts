cc09d76f71ae7096787a8e44ad1382ed04d5cd32
Nepomuk Seiler <muuki88@users.noreply.github.com>
2022-06-22 17:21:20 +0200

    Non-blocking adex loading (#55)
    * Non-blocking adex loading
    
    * fix boolean logic

--- modules/adex/index.ts
+++ modules/adex/index.ts
@@ -241,17 +241,16 @@ export class AdexModule implements IModule {
     ]);
 
     // load script if consent is given
-    return !this.hasRequiredConsent(context.tcData) || this.isLoaded
-      ? Promise.resolve()
-      : assetLoaderService
-          .loadScript({
-            name: this.name,
-            assetUrl: `https://dmp.theadex.com/d/${adexCustomerId}/${adexTagId}/s/adex.js`,
-            loadMethod: AssetLoadMethod.TAG
-          })
-          .then(_ => {
-            this.isLoaded = true;
-          });
+    if (this.hasRequiredConsent(context.tcData) && !this.isLoaded) {
+      this.isLoaded = true;
+      assetLoaderService.loadScript({
+        name: this.name,
+        assetUrl: `https://dmp.theadex.com/d/${adexCustomerId}/${adexTagId}/s/adex.js`,
+        loadMethod: AssetLoadMethod.TAG
+      });
+    }
+
+    return Promise.resolve();
   }
 
   hasRequiredConsent = (tcData: tcfapi.responses.TCData): boolean =>
