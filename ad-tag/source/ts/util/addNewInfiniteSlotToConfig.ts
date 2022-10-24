5cb061ed52969cd6e23ab9bd0ed73de5b79719f2
anitaschiller <77627311+anitaschiller@users.noreply.github.com>
2022-10-24 11:59:19 +0200

    GD-4472: Refactor infinite scrolling test and add documentation (#96)
    Co-authored-by: anita.schiller <anita.schiller@gutefrage.net>

--- ad-tag/source/ts/util/addNewInfiniteSlotToConfig.ts
+++ ad-tag/source/ts/util/addNewInfiniteSlotToConfig.ts
@@ -1,10 +1,19 @@
 import { getLogger } from './logging';
 import { Moli } from '../types/moli';
 
+/**
+ * Add a slot with a new domId and the configuration of the 'infinite' loading ad slot to the moli config
+ * @param config the moli config
+ * @param idOfConfiguredSlot the domId of the configured ad slot with an 'infinite' loading behaviour
+ * @param artificialIdOfNewSlot the artificial domId for the added newly added infinite slot
+ * @param window
+ */
+
 export const addNewInfiniteSlotToConfig = (
   config: Moli.MoliConfig,
   idOfConfiguredSlot: string,
-  artificialIdOfNewSlot: string
+  artificialIdOfNewSlot: string,
+  window: Window
 ): Moli.MoliConfig => {
   let newConfig = config;
 
