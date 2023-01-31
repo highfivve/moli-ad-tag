c1ffbc947f2767c64bf9037b46a6fd88983e795c
anita.schiller <anita.schiller@gutefrage.net>
2023-01-31 15:06:59 +0100

    GD-5824/GD-5826: Extend tests for extractDomainFromHostname function

--- ad-tag/source/ts/util/extractDomainFromHostname.test.ts
+++ ad-tag/source/ts/util/extractDomainFromHostname.test.ts
@@ -2,7 +2,7 @@ import { expect } from 'chai';
 import { extractDomainFromHostname } from './extractDomainFromHostname';
 
 describe('extractDomainFromHostname', () => {
-  it('should cut off subdomains like www. in the hostname', () => {
+  it('should cut off the subdomain like www. in the hostname', () => {
     const hostname = 'www.testdomain.de';
     expect(extractDomainFromHostname(hostname)).to.equal('testdomain.de');
   });
@@ -17,5 +17,17 @@ describe('extractDomainFromHostname', () => {
   it('should return undefined if the hostname is undefined', () => {
     const hostname = undefined;
     expect(extractDomainFromHostname(hostname)).to.equal(undefined);
+  });
+  it('should return the hostname as it is if there is no subdomain', () => {
+    const hostname = 'testdomain.de';
+    expect(extractDomainFromHostname(hostname)).to.equal(hostname);
+  });
+  it('should return the hostname as it is if there is no top level domain', () => {
+    const hostname = 'localhost';
+    expect(extractDomainFromHostname(hostname)).to.equal(hostname);
+  });
+  it('should remove ALL subdomains from the hostname', () => {
+    const hostname = 'foo.bar.www.testdomain.de';
+    expect(extractDomainFromHostname(hostname)).to.equal('testdomain.de');
   });
 });
