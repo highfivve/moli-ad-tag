9b5233aded2a11173fff5c618ae3d94244d56b47
Nepomuk Seiler <nepomuk.seiler@gutefrage.net>
2023-03-14 21:45:52 +0100

    GD-6165 cleanup uncommented code

--- ad-tag/source/ts/util/extractDomainFromHostname.ts
+++ ad-tag/source/ts/util/extractDomainFromHostname.ts
@@ -5,24 +5,18 @@
  *
  * @return string|undefined domain
  */
-
 export const extractDomainFromHostname = (hostname: string | undefined): string | undefined => {
   if (hostname) {
-    // UK top level domains
     const [tld, sub1, sub2] = hostname.split('.').reverse();
 
     switch (`${sub1}.${tld}`) {
+      // special country code TLDs
       case 'co.uk':
       case 'com.br':
         return `${sub2}.${sub1}.${tld}`;
       default:
         return sub1 ? `${sub1}.${tld}` : tld;
     }
-
-    // const apexDomain = hostname.substring(
-    //   hostname.lastIndexOf('.', hostname.lastIndexOf('.') - 1) + 1
-    // );
-    // return apexDomain;
   } else {
     return;
   }
