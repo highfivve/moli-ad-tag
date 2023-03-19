19bc77d6c2d85619759b02f58a77e0efa690f5a5
Muki Seiler <muuki88@users.noreply.github.com>
2023-03-19 20:10:14 +0100

    Update webpack (#122)

--- prebuilt/ad-tag/webpack.config.js
+++ prebuilt/ad-tag/webpack.config.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const path = require('path');
 const packageJson = require('./package.json');
 
 module.exports = (_, argv) => {
@@ -8,7 +7,7 @@ module.exports = (_, argv) => {
     mode: 'development',
     devtool: argv.mode === 'production' ? false : 'inline-source-map',
     entry: {
-      [`adtag.${packageJson.version}`] : './index.ts',
+      [`adtag.${packageJson.version}`]: './index.ts',
       latest: './index.ts'
     },
     output: {
@@ -20,18 +19,6 @@ module.exports = (_, argv) => {
           test: /\.ts$/,
           loader: 'ts-loader',
           options: { allowTsInNodeModules: false, projectReferences: true }
-        },
-        // this separate rule is required to make sure that the Prebid.js files are babel-ified.  this rule will
-        // override the regular exclusion from above (for being inside node_modules).
-        {
-          test: /.js$/,
-          include: new RegExp(`\\${path.sep}prebid\.js`),
-          use: {
-            loader: 'babel-loader',
-            // presets and plugins for Prebid.js must be manually specified separate from your other babel rule.
-            // this can be accomplished by requiring prebid's .babelrc.js file (requires Babel 7 and Node v8.9.0+)
-            options: require('prebid.js/.babelrc.js')
-          }
         }
       ]
     },
