0f8cd86105684760e78b20b64ae685cb8028498b
Nepomuk Seiler <nepomuk.seiler@gutefrage.net>
2023-11-28 19:40:55 +0100

    registerSignalSources can be undefined

--- ad-tag/source/ts/ads/prebid.ts
+++ ad-tag/source/ts/ads/prebid.ts
@@ -126,7 +126,12 @@ export const prebidConfigure = (
 
           // configure ESP for googletag. This has to be called after setConfig and after the googletag has loaded.
           // don't add this to the init step.
-          context.window.pbjs.registerSignalSources();
+          if (
+            context.window.pbjs.registerSignalSources &&
+            typeof context.window.pbjs.registerSignalSources === 'function'
+          ) {
+            context.window.pbjs.registerSignalSources();
+          }
 
           resolve();
         });
