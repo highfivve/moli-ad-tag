92dcef81cf3a3f46e6428f88f1e3de9852422108
Shadi <shadi.alsayed@gutefrage.net>
2024-03-28 12:34:37 +0100

    GD-6661: Disable globalAuctionContext by default

--- ad-tag/source/ts/ads/globalAuctionContext.ts
+++ ad-tag/source/ts/ads/globalAuctionContext.ts
@@ -3,7 +3,7 @@ import { Moli } from '../types/moli';
 export class GlobalAuctionContext {
   readonly enabled: boolean;
   constructor(
-    private readonly config: Moli.auction.GlobalAuctionContextConfig = { enabled: true }
+    private readonly config: Moli.auction.GlobalAuctionContextConfig = { enabled: false }
   ) {
     this.enabled = this.config.enabled;
   }
