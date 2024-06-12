7a50d2fa32161668cab7145626649d0e7f5e83a8
Nepomuk Seiler <nepomuk.seiler@gutefrage.net>
2024-06-12 22:10:41 +0200

    Add nodeResolve plugin to rollup example and separate prod and dev build

--- ad-tag/source/ts/bundle/pubstack.ts
+++ ad-tag/source/ts/bundle/pubstack.ts
@@ -1,5 +1,5 @@
 import type { MoliRuntime } from '../types/moliRuntime';
-import { Pubstack } from '../ads/modules/pubstack'; // rollup seems to require pubstack/index to properly resolve the module
+import { Pubstack } from '../ads/modules/pubstack';
 
 declare const window: MoliRuntime.MoliWindow;
 
--- examples/rollup/rollup.config.mjs
+++ examples/rollup/rollup.config.mjs
@@ -2,41 +2,38 @@ import typescript from '@rollup/plugin-typescript';
 import html, { makeHtmlAttributes } from "@rollup/plugin-html";
 import terser from "@rollup/plugin-terser";
 import serve from 'rollup-plugin-serve';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 
-// FIXME this mixes development and production settings. While the terser plugin should only be used in production,
-//       the html plugin and serve is only necessary for development.
-export default {
-  input: 'index.ts',
-  output: {
-    file: 'dist/moli.mjs',
-    format: 'es'
-  },
-  plugins: [ typescript(), terser(), html({
-      template: ({bundle, attributes, meta, files, publicPath, title}) => {
-        // copied from the default template and added files.mjs
-        // https://github.com/rollup/plugins/blob/master/packages/html/src/index.ts
-        const scripts = [ ...(files.js || []), ...(files.mjs || []) ]
-          .map(({fileName}) => {
-            const attrs = makeHtmlAttributes(attributes.script);
-            return `<script src="${publicPath}${fileName}"${attrs}></script>`;
-          })
-          .join('\n');
+const isProduction = process.env.BUILD_TARGET === 'production';
 
-        const links = (files.css || [])
-          .map(({fileName}) => {
-            const attrs = makeHtmlAttributes(attributes.link);
-            return `<link href="${publicPath}${fileName}" rel="stylesheet"${attrs}>`;
-          })
-          .join('\n');
+console.log('isProduction:', isProduction);
 
-        const metas = meta
-          .map((input) => {
-            const attrs = makeHtmlAttributes(input);
-            return `<meta${attrs}>`;
-          })
-          .join('\n');
+const htmlPlugin = html({
+  template: ({attributes, meta, files, publicPath, title}) => {
+    // copied from the default template and added files.mjs
+    // https://github.com/rollup/plugins/blob/master/packages/html/src/index.ts
+    const scripts = [ ...(files.js || []), ...(files.mjs || []) ]
+      .map(({fileName}) => {
+        const attrs = makeHtmlAttributes(attributes.script);
+        return `<script src="${publicPath}${fileName}"${attrs}></script>`;
+      })
+      .join('\n');
 
-        return `<!doctype html>
+    const links = (files.css || [])
+      .map(({fileName}) => {
+        const attrs = makeHtmlAttributes(attributes.link);
+        return `<link href="${publicPath}${fileName}" rel="stylesheet"${attrs}>`;
+      })
+      .join('\n');
+
+    const metas = meta
+      .map((input) => {
+        const attrs = makeHtmlAttributes(input);
+        return `<meta${attrs}>`;
+      })
+      .join('\n');
+
+    return `<!doctype html>
 <html${makeHtmlAttributes(attributes.html)}>
   <head>
     ${metas}
@@ -49,11 +46,19 @@ export default {
     ${scripts}
   </body>
 </html>`;
-      }
-    }
-  ),
-    serve({
-      contentBase: 'dist',
-      port: 8000,
-    }) ]
+  }
+});
+
+export default {
+  input: 'index.ts',
+  output: {
+    file: 'dist/moli.mjs',
+    format: 'es'
+  },
+  plugins: [ nodeResolve(), typescript(),
+    ...(isProduction ?
+        [ terser() ] :
+        [ htmlPlugin, serve({contentBase: 'dist', port: 8000,}) ]
+    )
+  ]
 };
