cca90b7490c33edf938aa82b2b7c2d0a5293c57c
Nepomuk Seiler <nepomuk.seiler@gutefrage.net>
2024-06-28 13:36:20 +0200

    Allow modules to be passed as varargs

--- bundle.ts
+++ bundle.ts
@@ -16,8 +16,12 @@ import { program } from 'commander';
 import { execSync } from 'node:child_process';
 
 type OptionsValues = {
+  /**
+   * a list of modules to be build
+   */
+  readonly modules: string[];
   readonly output: string;
-  readonly config: string;
+  readonly config?: string;
   readonly failAfterWarnings: boolean;
 };
 
@@ -33,7 +37,8 @@ const command = program
   .description('Create a bundle from the given modules.json file and the given output file')
   .version('1.0.0')
   .requiredOption('-o, --output <output>', 'output file')
-  .requiredOption(
+  .option('-m, --modules [...modules]', 'a list of modules that should be part of the ad tag bundle. The order of the modules is important. The `bundle/configureFromEndpoint` module must always be last. The config option will be ignored if modules are specified')
+  .option(
     '-c, --config <config>',
     'file containing the modules that should be part of the ad tag bundle',
     value => path.join(__dirname, value)
@@ -49,9 +54,23 @@ const command = program
 // parse modules.json
 const options = command.opts<OptionsValues>();
 
-const config = JSON.parse(fs.readFileSync(options.config, 'utf-8')) as BundleConfig;
+const selectedModules = (): string[] => {
+  if(options.modules && options.modules.length > 0) {
+    return options.modules;
+  } else if (options.config) {
+    if(fs.existsSync(options.config)) {
+      const config = JSON.parse(fs.readFileSync(options.config, 'utf-8')) as BundleConfig;
+      return config.modules;
+    }
+    program.error(`Config file does not exist: ${options.config}`);
+  }
 
-const modules = ['init', ...config.modules];
+  program.error('No modules or config file specified');
+}
+
+const modules = ['init', selectedModules()];
+
+console.log('Selected modules:', modules);
 
 // generate entrypoint file
 const entrypoint = path.join(__dirname, 'ad-tag', 'source', 'ts', 'bundle', 'bundle.ts');
