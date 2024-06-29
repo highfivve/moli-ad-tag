913457c81cb0c6cbba2734e31900f5599a71704d
Nepomuk Seiler <nepomuk.seiler@gutefrage.net>
2024-06-29 22:50:37 +0200

    Remove babel build

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,8 +1,6 @@
 import typescript from '@rollup/plugin-typescript';
 import terser from "@rollup/plugin-terser";
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-import { getBabelOutputPlugin } from "@rollup/plugin-babel";
-import path from "path";
 
 // input and output are passed from the command line
 export default {
@@ -10,24 +8,10 @@ export default {
     {
       file: 'dist/bundle.mjs',
       format: 'es',
-      plugins: [
-        getBabelOutputPlugin({
-          allowAllFormats: true,
-          configFile: path.resolve('./babel.es6.config.json')
-        }),
-        terser()
-      ]
     },
     {
       file: 'dist/bundle.es5.js',
-      format: 'iife',
-      plugins: [
-        getBabelOutputPlugin({
-          allowAllFormats: true,
-          configFile: path.resolve('./babel.es5.config.json')
-        }),
-        terser()
-      ]
+      format: 'iife'
     }
   ],
   plugins: [
