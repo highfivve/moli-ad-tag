85224110bc2bcf9902191192d4007a1351f96216
Nepomuk Seiler <nepomuk.seiler@gutefrage.net>
2024-07-05 21:31:41 +0200

    ADd requestAds setting and remove instant module from example

--- examples/esbuild/index.ts
+++ examples/esbuild/index.ts
@@ -1,5 +1,4 @@
 // depend on the ad-tag library that was build locally
 import '../../lib/bundle/init';
-import '../../lib/bundle/instant';
 import '../../lib/bundle/pubstack';
 import '../../lib/bundle/configureFromEndpoint';
