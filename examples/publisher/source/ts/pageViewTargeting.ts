d753eaa6c544d039e6ea376aa59ac482b52632d1
Nepomuk Seiler <nepomuk.seiler@gutefrage.net>
2024-07-10 15:56:06 +0200

    Delete unused examples

--- examples/publisher/source/ts/pageViewTargeting.ts
+++
@@ -1,9 +0,0 @@
-const pageViewKey = '__pageViews';
-
-export const getPageViewCount = (): string => {
-  const pageViewsStr = window.sessionStorage.getItem(pageViewKey) || '0';
-  const pageViews = Number.isInteger(pageViewsStr) ? Number.parseInt(pageViewsStr) : 0;
-  const nextPageViews = pageViews + 1;
-  window.sessionStorage.setItem(pageViewKey, nextPageViews.toString());
-  return (nextPageViews >= 5 ? 5 : nextPageViews).toString();
-};
