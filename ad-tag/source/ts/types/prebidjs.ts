2252e6280c80b148cb73c85f25cc13bfcf3dd106
Nepomuk Seiler <nepomuk.seiler@gutefrage.net>
2024-07-15 20:16:06 +0200

    Add missing ext property on pbsBidAdapter

--- ad-tag/source/ts/types/prebidjs.ts
+++ ad-tag/source/ts/types/prebidjs.ts
@@ -4228,11 +4228,13 @@ export namespace prebidjs {
     readonly params: IPrebidServerBidParams;
 
     readonly ortb2Imp: IOrtb2Imp & {
-      readonly prebid: IOrtb2ImpPrebid & {
-        /**
-         * stored request is required for prebid server calls
-         */
-        readonly storedrequest: IOrtb2ImpStoredRequest;
+      readonly ext?: {
+        readonly prebid: IOrtb2ImpPrebid & {
+          /**
+           * stored request is required for prebid server calls
+           */
+          readonly storedrequest: IOrtb2ImpStoredRequest;
+        };
       };
     };
   }
