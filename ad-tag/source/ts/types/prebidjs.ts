4485d482c0857bc86672e13d594f76dffa965738
Nepomuk Seiler <nepomuk.seiler@gutefrage.net>
2024-07-29 14:15:14 +0200

    GD-8122 Prettier

--- ad-tag/source/ts/types/prebidjs.ts
+++ ad-tag/source/ts/types/prebidjs.ts
@@ -4151,13 +4151,12 @@ export namespace prebidjs {
    *
    * @see https://docs.prebid.org/dev-docs/bidders/stailamedia.html
    */
-  type AppNexusAliases = typeof AppNexusAst | typeof AppNexus | typeof StailaMedia
+  type AppNexusAliases = typeof AppNexusAst | typeof AppNexus | typeof StailaMedia;
 
   /**
    * AppNexus bid object.
    */
-  export interface IAppNexusASTBid
-    extends IBidObject<AppNexusAliases, IAppNexusASTParams> {}
+  export interface IAppNexusASTBid extends IBidObject<AppNexusAliases, IAppNexusASTParams> {}
 
   export interface IGumGumParams {
     /**
