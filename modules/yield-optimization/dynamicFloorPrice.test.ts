78be6af318046b080bbede4e2fe50fe19427c057
anita.schiller <anita.schiller@gutefrage.net>
2024-09-23 16:54:43 +0200

    GD-6664: Update calculateDynamicPriceRule and tests

--- modules/yield-optimization/dynamicFloorPrice.test.ts
+++ modules/yield-optimization/dynamicFloorPrice.test.ts
@@ -1,115 +1,167 @@
 import { expect } from 'chai';
 
-import {
-  calculateDynamicFloorPrice,
-  calculateDynamicPriceRule,
-  previousMaxCpm,
-  previousMinCpm,
-  previousSecondHighestCpm
-} from './dynamicFloorPrice';
+import { calculateDynamicPriceRule } from './dynamicFloorPrice';
 import { Moli } from '@highfivve/ad-tag/lib/types/moli';
 import PriceRule = Moli.yield_optimization.PriceRule;
-import { DynamicFloorPriceStrategy } from './index';
 
 describe('dynamicFloorPrice', () => {
-  describe('previousMaxCpm', () => {
-    it('should return the highest bid cpm in cents', () => {
-      const bidCpms = [0.1, 2, 0.3, 1.1, 2.3];
-      expect(previousMaxCpm(bidCpms)).to.equal(230);
-    });
-    it('should return null if there was no bid', () => {
-      const bidCpms = [];
-      expect(previousMaxCpm(bidCpms)).to.equal(null);
-    });
-  });
-
-  describe('previousMinCpm', () => {
-    it('should return the lowest bid cpm in cents', () => {
-      const bidCpms = [0.1, 2, 0.3, 1.1, 2.3];
-      expect(previousMinCpm(bidCpms)).to.equal(10);
-    });
-    it('should return null if there was no bid ', () => {
-      const bidCpms = [];
-      expect(previousMinCpm(bidCpms)).to.equal(null);
-    });
-  });
-
-  describe('previousSecondHighestCpm', () => {
-    it('should return the second highest bid cpm in cents', () => {
-      const bidCpms = [0.1, 2, 0.3, 1.1, 2.3];
-      expect(previousSecondHighestCpm(bidCpms)).to.equal(200);
-    });
-    it('should return null if there was no bid ', () => {
-      const bidCpms = [];
-      expect(previousSecondHighestCpm(bidCpms)).to.equal(null);
-    });
-  });
-
-  describe('calculateDynamicFloorPrice', () => {
-    const strategies: DynamicFloorPriceStrategy[] = ['max', 'min', 'second-highest'];
-    strategies.forEach(strategy =>
-      it(`should return null if there was no bid for strategy ${strategy}`, () => {
-        expect(calculateDynamicFloorPrice(strategy, [])).to.equal(null);
-      })
-    );
-    it('should return the highest cpm of all bids if strategy is max', () => {
-      const strategy = 'max';
-      const bidCpms = [0.3, 1.43, 1.44, 0.8];
-      expect(calculateDynamicFloorPrice(strategy, bidCpms)).to.equal(144);
-    });
-    it('should return the lowest cpm of all bids if strategy is min', () => {
-      const strategy = 'min';
-      const bidCpms = [0.3, 1.43, 1.44, 0.8];
-      expect(calculateDynamicFloorPrice(strategy, bidCpms)).to.equal(30);
-    });
-    it('should return the second highest cpm of all bids if strategy is second-highest', () => {
-      const strategy = 'second-highest';
-      const bidCpms = [0.3, 1.43, 1.44, 0.8];
-      expect(calculateDynamicFloorPrice(strategy, bidCpms)).to.equal(143);
-    });
-  });
-
   describe('calculateDynamicPriceRule', () => {
+    const standardRule: PriceRule = {
+      floorprice: 0.5,
+      priceRuleId: 1564,
+      model: 'static',
+      main: true
+    };
     it('should return the standardRule rule if there is no list of previous cpms', () => {
       const strategy = 'max';
       const bidCpms = undefined;
-      const standardRule: PriceRule = {
-        floorprice: 0.5,
-        priceRuleId: 1564,
-        model: 'static',
-        main: true
-      };
-      expect(calculateDynamicPriceRule(strategy, bidCpms, standardRule)).to.deep.equal(
+      expect(calculateDynamicPriceRule(strategy, bidCpms, standardRule, 5, 500)).to.deep.equal(
         standardRule
       );
     });
     it('should return the standardRule rule if there was no bid in the previous auction', () => {
       const strategy = 'max';
       const bidCpms = [];
-      const standardRule: PriceRule = {
-        floorprice: 0.5,
-        priceRuleId: 1564,
-        model: 'static',
-        main: true
-      };
-      expect(calculateDynamicPriceRule(strategy, bidCpms, standardRule)).to.deep.equal(
+      expect(calculateDynamicPriceRule(strategy, bidCpms, standardRule, 5, 500)).to.deep.equal(
         standardRule
       );
     });
-    it('should return the updated rule if there was a bid', () => {
+    it('should return the standardRule if there are only negative numbers in the bidCpms array', () => {
       const strategy = 'max';
-      const bidCpms = [0.3, 1.43, 1.44, 0.8];
-      const standardRule: PriceRule = {
-        floorprice: 0.5,
-        priceRuleId: 1564,
-        model: 'static',
-        main: true
-      };
-      expect(calculateDynamicPriceRule(strategy, bidCpms, standardRule)).to.deep.equal({
-        floorprice: 1.44,
-        priceRuleId: 144,
-        model: 'static',
-        main: true
+      const bidCpms = [-0.3, -1.43, -1.44, -5.8];
+      expect(calculateDynamicPriceRule(strategy, bidCpms, standardRule, 5, 500)).to.deep.equal(
+        standardRule
+      );
+    });
+    it('should return the standardRule if there are only values under 10 cents in the bidCpms array', () => {
+      const strategy = 'max';
+      const bidCpms = [0, 0.005, 0, 0.08];
+      expect(calculateDynamicPriceRule(strategy, bidCpms, standardRule, 5, 500)).to.deep.equal(
+        standardRule
+      );
+    });
+    it('should return the standardRule if there are only bids above 30 euros (that is where we set the validity "limit")', () => {
+      const strategy = 'max';
+      const bidCpms = [Number.MAX_VALUE, Number.MAX_SAFE_INTEGER, 45];
+      expect(calculateDynamicPriceRule(strategy, bidCpms, standardRule, 5, 500)).to.deep.equal(
+        standardRule
+      );
+    });
+    it('should return the standardRule if there are only bids below 10 cents (upr_id at GAM starts at 10 cents)', () => {
+      const strategy = 'max';
+      const bidCpms = [Number.MIN_VALUE, Number.MIN_SAFE_INTEGER, 0.09];
+      expect(calculateDynamicPriceRule(strategy, bidCpms, standardRule, 5, 500)).to.deep.equal(
+        standardRule
+      );
+    });
+    it('should return the standardRule if there are only NaN bid cpms', () => {
+      const strategy = 'max';
+      const bidCpms = [NaN];
+      expect(calculateDynamicPriceRule(strategy, bidCpms, standardRule, 5, 500)).to.deep.equal(
+        standardRule
+      );
+    });
+    describe('when strategy is "max"', () => {
+      it('should return the updated and rounded down rule of the highest bid if there was a bid below the maxFloorInCents', () => {
+        const strategy = 'max';
+        const bidCpms = [0.3, 1.43, 1.44, 0.8];
+        expect(calculateDynamicPriceRule(strategy, bidCpms, standardRule, 5, 500)).to.deep.equal({
+          floorprice: 1.44,
+          priceRuleId: 140,
+          model: 'static',
+          main: true
+        });
+      });
+      it('should return the updated and rounded down rule of the highest bid if there was at least one cpm above 10 cents', () => {
+        const strategy = 'max';
+        const bidCpms = [0, 0, 0.16];
+        expect(calculateDynamicPriceRule(strategy, bidCpms, standardRule, 5, 500)).to.deep.equal({
+          floorprice: 0.16,
+          priceRuleId: 15,
+          model: 'static',
+          main: true
+        });
+      });
+      it('should return the updated rule with the highest positive bid cpm', () => {
+        const strategy = 'max';
+        const bidCpms = [-0.3, 1.43, 1.44, -5.8];
+        expect(calculateDynamicPriceRule(strategy, bidCpms, standardRule, 5, 500)).to.deep.equal({
+          floorprice: 1.44,
+          priceRuleId: 140,
+          model: 'static',
+          main: true
+        });
+      });
+      it('should return the maxFloorInCents as new price rule if there was a bid above the maxFloorInCents', () => {
+        const strategy = 'max';
+        const bidCpms = [0.3, 1.43, 1.44, 5.8];
+        expect(calculateDynamicPriceRule(strategy, bidCpms, standardRule, 5, 500)).to.deep.equal({
+          floorprice: 5.8,
+          priceRuleId: 500,
+          model: 'static',
+          main: true
+        });
+      });
+    });
+    describe('when strategy is "min"', () => {
+      it('should return the updated and rounded down rule of the lowest bid if there was a bid below the maxFloorInCents', () => {
+        const strategy = 'min';
+        const bidCpms = [0.33, 1.43, 1.44, 0.8];
+        expect(calculateDynamicPriceRule(strategy, bidCpms, standardRule, 5, 500)).to.deep.equal({
+          floorprice: 0.33,
+          priceRuleId: 30,
+          model: 'static',
+          main: true
+        });
+      });
+      it('should return the updated and rounded down rule of the lowest bid if there was at least one cpm above 10 cents', () => {
+        const strategy = 'min';
+        const bidCpms = [0, 0.21, 0.16];
+        expect(calculateDynamicPriceRule(strategy, bidCpms, standardRule, 5, 500)).to.deep.equal({
+          floorprice: 0.16,
+          priceRuleId: 15,
+          model: 'static',
+          main: true
+        });
+      });
+      it('should return the updated rule with the lowest positive bid cpm', () => {
+        const strategy = 'min';
+        const bidCpms = [-0.3, 1.43, 1.44, -5.8];
+        expect(calculateDynamicPriceRule(strategy, bidCpms, standardRule, 5, 500)).to.deep.equal({
+          floorprice: 1.43,
+          priceRuleId: 140,
+          model: 'static',
+          main: true
+        });
+      });
+    });
+    describe('when strategy is "second-highest"', () => {
+      it('should return the updated and rounded down rule of the second-highest bid if there was a bid below the maxFloorInCents', () => {
+        const strategy = 'second-highest';
+        const bidCpms = [0.33, 1.43, 1.44, 0.8];
+        expect(calculateDynamicPriceRule(strategy, bidCpms, standardRule, 5, 500)).to.deep.equal({
+          floorprice: 1.43,
+          priceRuleId: 140,
+          model: 'static',
+          main: true
+        });
+      });
+      it('should return the standardRule if there was no valid second-highest bid', () => {
+        const strategy = 'second-highest';
+        const bidCpms = [0, 0.21];
+        expect(calculateDynamicPriceRule(strategy, bidCpms, standardRule, 5, 500)).to.deep.equal(
+          standardRule
+        );
+      });
+      it('should return the updated rule with the second-highest positive bid cpm', () => {
+        const strategy = 'second-highest';
+        const bidCpms = [-0.3, 1.43, 1.44, -5.8];
+        expect(calculateDynamicPriceRule(strategy, bidCpms, standardRule, 5, 500)).to.deep.equal({
+          floorprice: 1.43,
+          priceRuleId: 140,
+          model: 'static',
+          main: true
+        });
       });
     });
   });
--- modules/yield-optimization/dynamicFloorPrice.ts
+++ modules/yield-optimization/dynamicFloorPrice.ts
@@ -2,7 +2,7 @@ import { Moli } from '@highfivve/ad-tag/lib/types/moli';
 import PriceRule = Moli.yield_optimization.PriceRule;
 import { DynamicFloorPriceStrategy } from './index';
 
-export const previousMaxCpm = (cpms: number[]): null | number => {
+const previousMaxCpm = (cpms: number[]): null | number => {
   if (cpms.length === 0) {
     return null;
   } else {
@@ -11,7 +11,7 @@ export const previousMaxCpm = (cpms: number[]): null | number => {
   }
 };
 
-export const previousMinCpm = (cpms: number[]): null | number => {
+const previousMinCpm = (cpms: number[]): null | number => {
   if (cpms.length === 0) {
     return null;
   } else {
@@ -20,7 +20,7 @@ export const previousMinCpm = (cpms: number[]): null | number => {
   }
 };
 
-export const previousSecondHighestCpm = (cpms: number[]): null | number => {
+const previousSecondHighestCpm = (cpms: number[]): null | number => {
   if (!cpms || cpms.length === 0) {
     return null;
   } else {
@@ -29,7 +29,7 @@ export const previousSecondHighestCpm = (cpms: number[]): null | number => {
   }
 };
 
-export const calculateDynamicFloorPrice = (
+const calculateDynamicFloorPrice = (
   strategy: DynamicFloorPriceStrategy | undefined,
   previousCpms: number[]
 ): number | null => {
@@ -45,21 +45,84 @@ export const calculateDynamicFloorPrice = (
   }
 };
 
+/**
+ * Rounds down a price in cents to the nearest multiple of the given rounding step in cents.
+ * Ensures the rounded price does not exceed the max floor price if provided.
+ *
+ * @param priceInCents - The price in cents to be rounded down.
+ * @param roundingStepInCents - The rounding step in cents.
+ * @param [maxFloorInCents] - The maximum floor price in cents.
+ * @returns roundedPriceInCents - The rounded down price in cents.
+ */
+const roundDownPrice = (
+  priceInCents: number,
+  roundingStepInCents: number,
+  maxFloorInCents?: number
+): number => {
+  const roundedPriceInCents = Math.floor(priceInCents / roundingStepInCents) * roundingStepInCents;
+  if (maxFloorInCents !== undefined && roundedPriceInCents > maxFloorInCents) {
+    return maxFloorInCents;
+  }
+  return roundedPriceInCents;
+};
+
+/**
+ * Calculates a dynamic price rule based on the provided strategy and previous bid CPMs.
+ * The function
+ *  - filters out invalid CPMs (negative values, values under 10 cents, values above 30 euros, NaN)
+ *  - calculates the new floor price based on the strategy
+ *  - rounds it down to the nearest multiple of the given rounding step
+ *  - ensures the rounded price does not exceed the maximum floor price if provided
+ *
+ *  This function is needed to overwrite the standard price rule from the yield optimization (which could e.g. be 0.50 EUR floor price independent of current bids on the position)
+ *  with a dynamic floor price based on previous bids on the position.
+ *
+ * ! Caution: everything here assumes EUR as a currency and does not work with other currencies
+ *  - EUR as currency has to be configured in prebid!
+ *
+ * @param strategy - The strategy to use for calculating the dynamic floor price. Can be 'max', 'min', or 'second-highest'.
+ * @param previousCpms - An array of previous CPM values to base the calculation on (currently only available for prebid).
+ * @param standardRule - The standard price rule to fall back on if no valid CPMs are provided or the strategy is undefined.
+ * @param roundingStepsInCents - The rounding step in cents to round down the calculated floor price (upr_ids in GAM is set in 5 cent steps atm).
+ * @param [maxFloorInCents] - The optional maximum price rule in cents. If the rounded price rule exceeds this value, it will be capped at this maximum (max. configured upr_id value is 500 cents atm).
+ * @returns {PriceRule} - The calculated dynamic price rule with the updated floor price and price rule ID - each upr_id addresses a price rule (floor price) in GAM.
+ *
+ * @example
+ * const strategy = 'max';
+ * const previousCpms = [0.3, 1.43, 1.44, 0.8];
+ * const standardRule = { floorprice: 1.0, priceRuleId: 100, model: 'static', main: true };
+ * const roundingStepsInCents = 5;
+ * const maxFloorInCents = 500;
+ * const result = calculateDynamicPriceRule(strategy, previousCpms, standardRule, roundingStepsInCents, maxFloorInCents);
+ * // result: { floorprice: 1.44, priceRuleId: 140, model: 'static', main: true }
+ */
 export const calculateDynamicPriceRule = (
   strategy: DynamicFloorPriceStrategy | undefined,
   previousCpms: number[] | undefined,
-  standardRule: PriceRule
+  standardRule: PriceRule,
+  roundingStepsInCents: number,
+  maxFloorInCents?: number
 ): PriceRule => {
-  if (previousCpms && strategy) {
-    const calculatedFloorPrice = calculateDynamicFloorPrice(strategy, previousCpms);
-    return {
-      ...standardRule,
-      // floorprice in euros
-      floorprice: calculatedFloorPrice ? calculatedFloorPrice / 100 : standardRule.floorprice,
-      // price rule id in cents
-      priceRuleId: calculatedFloorPrice ? calculatedFloorPrice : standardRule.priceRuleId
-    };
-  } else {
+  // upr_ids start at 10 cents / we consider bids above 30 euros as unrealistic and invalid
+  const validPreviousCpms = previousCpms?.filter(cpm => cpm >= 0.1 && cpm < 30 && !isNaN(cpm));
+  if (!validPreviousCpms || validPreviousCpms.length === 0 || !strategy) {
     return standardRule;
   }
+
+  const calculatedFloorPrice = calculateDynamicFloorPrice(strategy, validPreviousCpms);
+  if (!calculatedFloorPrice) {
+    return standardRule;
+  }
+
+  const roundedPriceRuleIdInCents = roundDownPrice(
+    calculatedFloorPrice,
+    roundingStepsInCents,
+    maxFloorInCents
+  );
+
+  return {
+    ...standardRule,
+    floorprice: calculatedFloorPrice / 100,
+    priceRuleId: roundedPriceRuleIdInCents
+  };
 };
