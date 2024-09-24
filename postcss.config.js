9559ae0f8f9435a773c63932a07489b4d2e42715
Nepomuk Seiler <nepomuk.seiler@gutefrage.net>
2024-09-24 11:32:14 +0200

    GD-5467 add all highfivve styles to ad tag

--- postcss.config.js
+++ postcss.config.js
@@ -2,12 +2,14 @@
 const config = {
   plugins: [
     require('autoprefixer'),
+    require('postcss-import'),
     require('postcss-nested'),
-    require('postcss-custom-properties')({ preserve: false }),
+    require('postcss-custom-properties')({ preserve: true }),
     require('postcss-custom-media')({}),
     require('postcss-extend')({}),
     require('postcss-color-function')({}),
     require('cssnano')({
+      preset: 'default',
       zindex: false, // prevents automatic postprocessing of the z-indexes, so we can define them as we like.
       autoprefixer: { add: false, remove: false }
     }),
