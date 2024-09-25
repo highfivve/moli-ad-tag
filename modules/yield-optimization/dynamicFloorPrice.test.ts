69db2dd4f674f03ec7645f9b863a32629e31b586
anita.schiller <anita.schiller@gutefrage.net>
2024-09-25 13:31:19 +0200

    GD-6664: Remove outdated test

--- modules/yield-optimization/dynamicFloorPrice.test.ts
+++ modules/yield-optimization/dynamicFloorPrice.test.ts
@@ -41,25 +41,13 @@ describe('dynamicFloorPrice', () => {
       );
     });
     describe('when strategy is "max"', () => {
-      it('should return the updated and rounded down rule of the highest bid if there was a bid below the maxFloorInCents', () => {
+      it('should return the rounded down price rule of the highest valid bid', () => {
         const strategy = 'max';
         const bidCpms = [0.3, 1.43, 1.44, 0.8];
         expect(calculateDynamicPriceRule(strategy, bidCpms, standardRule, 5, 500, 5)).to.deep.equal(
           {
             floorprice: 1.44,
             priceRuleId: 140,
-            model: 'static',
-            main: true
-          }
-        );
-      });
-      it('should return the updated and rounded down rule of the highest bid if there was at least one cpm above 10 cents', () => {
-        const strategy = 'max';
-        const bidCpms = [0, 0, 0.16];
-        expect(calculateDynamicPriceRule(strategy, bidCpms, standardRule, 5, 500, 5)).to.deep.equal(
-          {
-            floorprice: 0.16,
-            priceRuleId: 15,
             model: 'static',
             main: true
           }
@@ -77,7 +65,7 @@ describe('dynamicFloorPrice', () => {
           }
         );
       });
-      it('should return the maxFloorInCents as new price rule if there was a bid above the maxFloorInCents', () => {
+      it('should return the maxPriceRuleInCents as new price rule if there was a bid above the maxPriceRuleInCents', () => {
         const strategy = 'max';
         const bidCpms = [0.3, 1.43, 1.44, 5.8];
         expect(calculateDynamicPriceRule(strategy, bidCpms, standardRule, 5, 500, 5)).to.deep.equal(
@@ -89,7 +77,7 @@ describe('dynamicFloorPrice', () => {
           }
         );
       });
-      it('should return the maxFloorInCents as new price rule if there was a very huge bid above the maxFloorInCents', () => {
+      it('should return the maxPriceRuleInCents as new price rule if there was a very huge bid above the maxPriceRuleInCents', () => {
         const strategy = 'max';
         const bidCpms = [Number.MAX_VALUE, Number.MAX_SAFE_INTEGER];
         expect(calculateDynamicPriceRule(strategy, bidCpms, standardRule, 5, 500, 5)).to.deep.equal(
@@ -103,7 +91,7 @@ describe('dynamicFloorPrice', () => {
       });
     });
     describe('when strategy is "min"', () => {
-      it('should return the updated and rounded down rule of the lowest valid bid', () => {
+      it('should return the rounded down price rule of the lowest valid bid', () => {
         const strategy = 'min';
         const bidCpms = [0, 0.21, 0.16];
         expect(calculateDynamicPriceRule(strategy, bidCpms, standardRule, 5, 500, 5)).to.deep.equal(
@@ -127,7 +115,7 @@ describe('dynamicFloorPrice', () => {
           }
         );
       });
-      it('should return the minFloorInCents if there are only valid bids below this value', () => {
+      it('should return the minPriceRuleInCents if there are only valid bids below this value', () => {
         const strategy = 'min';
         const bidCpms = [0.09, 0.01];
         expect(calculateDynamicPriceRule(strategy, bidCpms, standardRule, 5, 500, 5)).to.deep.equal(
@@ -139,7 +127,7 @@ describe('dynamicFloorPrice', () => {
           }
         );
       });
-      it('should return the minFloorInCents if there are super small bids above 0', () => {
+      it('should return the minPriceRuleInCents if there are super small bids above 0', () => {
         const strategy = 'min';
         const bidCpms = [Number.MIN_VALUE, Number.MIN_SAFE_INTEGER];
         expect(calculateDynamicPriceRule(strategy, bidCpms, standardRule, 5, 500, 5)).to.deep.equal(
@@ -153,7 +141,7 @@ describe('dynamicFloorPrice', () => {
       });
     });
     describe('when strategy is "second-highest"', () => {
-      it('should return the updated and rounded down rule of the second-highest bid if there was a bid below the maxFloorInCents', () => {
+      it('should return the rounded down price rule of the second-highest bid', () => {
         const strategy = 'second-highest';
         const bidCpms = [0.33, 1.43, 1.44, 0.8];
         expect(calculateDynamicPriceRule(strategy, bidCpms, standardRule, 5, 500, 5)).to.deep.equal(
@@ -177,12 +165,24 @@ describe('dynamicFloorPrice', () => {
           }
         );
       });
-      it('should return the updated rule with the second-highest positive bid cpm', () => {
+      it('should return the updated price rule with the second-highest positive bid cpm', () => {
         const strategy = 'second-highest';
         const bidCpms = [-0.3, 1.43, 1.44, -5.8];
         expect(calculateDynamicPriceRule(strategy, bidCpms, standardRule, 5, 500, 5)).to.deep.equal(
           {
             floorprice: 1.43,
+            priceRuleId: 140,
+            model: 'static',
+            main: true
+          }
+        );
+      });
+      it('should return the updated price rule with the only positive bid cpm in case there is only one', () => {
+        const strategy = 'second-highest';
+        const bidCpms = [-0.3, 1.44, -5.8];
+        expect(calculateDynamicPriceRule(strategy, bidCpms, standardRule, 5, 500, 5)).to.deep.equal(
+          {
+            floorprice: 1.44,
             priceRuleId: 140,
             model: 'static',
             main: true
