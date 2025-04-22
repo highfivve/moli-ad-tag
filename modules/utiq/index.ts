8e5d6701909e54d62d773b892ffc081b55bbc518
Nepomuk Seiler <nepomuk.seiler@gutefrage.net>
2025-04-22 11:47:58 +0200

    Add utiq command queue

--- modules/utiq/index.ts
+++ modules/utiq/index.ts
@@ -164,6 +164,106 @@ export interface UtiqConfigOptions {
   readonly consentManagerDataLayer?: boolean;
 }
 
+type UtiqCommand = () => void;
+
+/**
+ * @see https://docs.utiq.com/docs/event-listeners#EventListeners-onConsentManagerStatusChanged
+ */
+type UtiqStatus = 'utiq_popup_shown' | 'utiq_popup_accepted' | 'utiq_popup_rejected';
+
+/**
+ * category will return 'mobile' or 'fixed', to differentiate if Utiq IDs are generated based the
+ * mobile connection, or the fixed (household) connection.
+ *
+ * @see https://docs.utiq.com/docs/event-listeners#EventListeners-onIdsAvailable
+ */
+interface UtiqIdsAvailableEvent {
+  mtid: string;
+  atid: string;
+  attrid: string;
+  category: 'mobile' | 'fixed';
+  ttl: string;
+  domain: string;
+}
+
+/**
+ * @see https://docs.utiq.com/docs/event-listeners
+ */
+interface UtiqEventMap {
+  /**
+   * This event is dispatched when the Utiq is fully initialized on every page load or navigation.
+   *
+   * @see https://docs.utiq.com/docs/event-listeners#EventListeners-onInitialised
+   */
+  onInitialised: () => void;
+
+  /**
+   * This event is dispatched when user eligibility check information is performed for the current client sending it
+   * with the parameter. The check occurs on initial page load, when the eligibility is validated before the Utiq loads,
+   * and on the consent acceptance.
+   *
+   * @see https://docs.utiq.com/docs/event-listeners#EventListeners-onEligibilityChecked
+   */
+  onEligibilityChecked: (event: { isEligible: boolean }) => void;
+
+  /**
+   * This event is dispatched when the Utiq gets signal from the browser (or client) to change the consent status to
+   * the one held with the parameter. The fact that the event is dispatched does not mean the consent will be changed -
+   * only that the signal has been sent. Further flow execution can be stopped by e.g. feedback that the consent already
+   * has the specified value
+   *
+   * @see https://docs.utiq.com/docs/event-listeners#EventListeners-onConsentChanging
+   */
+  onConsentChanging: (event: { isConsentGranted: boolean }) => void;
+
+  /**
+   * This event is dispatched when Utiq consent status update has finished to the one held with the parameter.
+   *
+   * @see https://docs.utiq.com/docs/event-listeners#EventListeners-onConsentUpdateFinished
+   */
+  onConsentUpdateFinished: (event: { isConsentGranted: boolean }) => void;
+
+  /**
+   * The event is dispatched each time the status of consent is changed. The possible values can be:
+   *
+   * - `utiq_popup_shown`
+   * - `utiq_popup_accepted`
+   * - `utiq_popup_rejected`
+   *
+   * The event is dispatched on the following situations:
+   *
+   * - when consent manager popup is shown to the user (utiq_popup_shown).
+   * - when consent manager popup is accepted (utiq_popup_accepted).
+   * - when consent manager popup is rejected (utiq_popup_rejected).
+   *
+   * @see https://docs.utiq.com/docs/event-listeners#EventListeners-onConsentManagerStatusChanged
+   */
+  onConsentManagerStatusChanged: (event: { status: UtiqStatus }) => void;
+
+  /**
+   * This event is dispatched when Utiq's mtid and atid are available for use and provides them via its parameters.
+   * It happens when the full Utiq flow is executed successfully and on the subsequent page loads when the IDs are
+   * already set up.
+   *
+   * @see https://docs.utiq.com/docs/event-listeners#EventListeners-onIdsAvailable
+   */
+  onIdsAvailable: (event: UtiqIdsAvailableEvent) => void;
+
+  /**
+   * This event is dispatched when Utiq has completed its flow, either user was eligible and accepted/rejected, user
+   * had accepted/rejected on previous session, or user was not eligible.
+   *
+   * Use case would be to use this event listener if you want to call other solutions as soon as Utiq flow ends, e.g.
+   * not calling Prebid after CMP but wait to call it when this event listener fires.
+   *
+   * @see https://docs.utiq.com/docs/event-listeners#EventListeners-onFlowCompleted
+   */
+  onFlowCompleted: () => void;
+}
+
+type UtiqEventType = keyof UtiqEventMap;
+type UtiqEventListener<T extends UtiqEventType> = (event: UtiqEventMap[T]) => void;
+
 /**
  * The Utiq API object.
  *
@@ -179,10 +279,34 @@ export interface UtiqAPI {
    * @see https://docs.utiq.com/docs/api-methods#APIMethods-showConsentManager
    */
   showConsentManager(): void;
+
+  /**
+   *
+   * @param event
+   * @param listener
+   * @see https://docs.utiq.com/docs/event-listeners
+   */
+  addEventListener<T extends UtiqEventType>(event: T, listener: UtiqEventListener<T>): void;
+
+  /**
+   *
+   * @param event
+   * @param listener
+   * @see https://docs.utiq.com/docs/event-listeners
+   */
+  removeEventListener<T extends UtiqEventType>(event: T, listener: UtiqEventListener<T>): void;
+
+  /**
+   * @param entryName - Name of the entry that should be extracted from the ID graph.
+   *                  Accepted values are: `mtid`, `atid`, `attrid`, `category`, `ttl`, `domain`
+   * @see https://docs.utiq.com/docs/api-methods#APIMethods-getIdGraphEntry
+   */
+  getIdGraphEntry(entryName: string): string;
 }
 
 export type UtiqWindow = {
   Utiq?: {
+    queue: Pick<UtiqCommand[], 'push'>;
     /**
      * The Utiq loader script can be configured using the Utiq.config object. Will be set from the config options provided
      * in the module configuration.
@@ -272,7 +396,11 @@ export class Utiq implements IModule {
     // merge any existing object. Existing configurations take precedence.
     utiqWindow.Utiq = utiqWindow.Utiq
       ? { ...utiqWindow.Utiq, config: { ...utiqWindow.Utiq.config, ...this.utiqConfig.options } }
-      : { config: this.utiqConfig.options };
+      : { queue: [], config: this.utiqConfig.options };
+
+    // double check queue initialization as publishers might already have set options, but did not
+    // properly initialize the queue
+    utiqWindow.Utiq.queue = utiqWindow.Utiq.queue || [];
 
     // no consent if gdpr applies
     if (
