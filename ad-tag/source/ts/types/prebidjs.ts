7ce754c675fb992919cda47a908272852155ef9a
Muki Seiler <muuki88@users.noreply.github.com>
2025-04-22 20:16:10 +0200

    Add publisherId for azerion / improve (#398)

--- ad-tag/source/ts/types/prebidjs.ts
+++ ad-tag/source/ts/types/prebidjs.ts
@@ -4353,7 +4353,15 @@ export namespace prebidjs {
    * @see https://prebid.org/dev-docs/bidders/improvedigital.html
    */
   export interface IImproveDigitalParams {
+    /**
+     * The placement ID from Improve Digital.
+     */
     readonly placementId: number;
+
+    /**
+     * The publisher ID from Improve Digital.
+     */
+    readonly publisherId: number;
     /**
      * Optional field to add additional targeting values.
      * Arbitrary keys can be added. The value is always a string array.
