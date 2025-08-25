2fa06547f5c176cab30f77cda109178b5bde0a7d
anita.schiller <anita.schiller@gutefrage.net>
2025-08-25 17:28:57 +0200

    GD-9609: Call function in interstitialModule.ts

--- ad-tag/source/ts/bundle/interstitialModule.ts
+++ ad-tag/source/ts/bundle/interstitialModule.ts
@@ -1,5 +1,5 @@
 import type { MoliRuntime } from '../types/moliRuntime';
-import { InterstitialModule } from 'ad-tag/ads/modules/interstitial';
+import { createInterstitialModule } from 'ad-tag/ads/modules/interstitial';
 
 declare const window: MoliRuntime.MoliWindow;
-window.moli.registerModule(new InterstitialModule());
+window.moli.registerModule(createInterstitialModule());
