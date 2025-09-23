0c27ac620231119dfde3492d3c86b7500158a4a2
Emil Nadimanov <emil.nadimanov@gutefrage.net>
2025-09-23 16:14:51 +0200

    id5: set id5 usersync config based on hem in runtime config

---
+++ ad-tag/source/ts/ads/modules/prebid-first-party-data/id5.ts
@@ -0,0 +1,69 @@
+import { MoliRuntime } from 'ad-tag/types/moliRuntime';
+import MoliRuntimeConfig = MoliRuntime.MoliRuntimeConfig;
+import { prebidjs } from 'ad-tag/types/prebidjs';
+import IID5Provider = prebidjs.userSync.IID5Provider;
+
+
+type Id5PartnerData = {
+  /**
+   * SHA256 Hashed Email
+   */
+  readonly 1: string;
+
+  /**
+   * SHA256 Hashed Phone Number
+   */
+  readonly 2?: string;
+
+  /**
+   * IPv4 Address of the end-user’s device
+   */
+  readonly 10?: string;
+
+  /**
+   * IPv6 Address of the end-user’s device
+   */
+  readonly 11?: string;
+};
+
+const createPd = (runtimeConfig: MoliRuntimeConfig): string | null => {
+  const sha256Email = runtimeConfig.audience?.hem?.sha256;
+  if (sha256Email) {
+    // this is from the ID5 documentation
+    // set the keys and URL-encode each value
+    const pdKeys: Id5PartnerData = {
+      1: sha256Email
+    };
+
+    // convert the key/values into a querystring format
+    const pdRaw = Object.entries(pdKeys)
+      .map(([key, value]) => {
+        return `${key}=${encodeURIComponent(value)}`;
+      })
+      .join('&');
+    return btoa(pdRaw);
+  }
+  return null;
+};
+
+/**
+ *
+ * @param runtimeConfig - runtime parameters that can be set by publisher
+ * @see https://wiki.id5.io/identitycloud/retrieve-id5-ids/passing-partner-data-to-id5
+ */
+export const id5Config = (runtimeConfig: MoliRuntimeConfig): IID5Provider => {
+  const pd = createPd(runtimeConfig);
+  return {
+    name: 'id5Id',
+    storage: {
+      type: 'html5',
+      name: 'id5id', // create a cookie with this name
+      expires: 90, // local storage entry lasts for 90 days
+      refreshInSeconds: 8 * 3600 // refresh ID every 8 hours to ensure it is fresh
+    },
+    params: {
+      partner: 1519,
+      ...(pd && { pd })
+    }
+  };
+};
--- ad-tag/source/ts/ads/prebid.ts
+++ ad-tag/source/ts/ads/prebid.ts
@@ -23,12 +23,13 @@ import { SupplyChainObject } from '../types/supplyChainObject';
 import { resolveStoredRequestIdInOrtb2Object } from '../util/resolveStoredRequestIdInOrtb2Object';
 import { createTestSlots } from '../util/test-slots';
 import { AssetLoadMethod, IAssetLoaderService } from '../util/assetLoaderService';
-import IPrebidJs = prebidjs.IPrebidJs;
 import { AdServer, AdSlot, headerbidding, schain } from '../types/moliConfig';
 import { packageJson } from 'ad-tag/gen/packageJson';
 import { prebidOutstreamRenderer } from 'ad-tag/ads/prebid-outstream';
 import { isGamInterstitial } from 'ad-tag/ads/auctions/interstitialContext';
 import { criteoEnrichWithFpd } from 'ad-tag/ads/modules/prebid-first-party-data/criteo';
+import { id5Config } from 'ad-tag/ads/modules/prebid-first-party-data/id5';
+import IPrebidJs = prebidjs.IPrebidJs;
 
 // if we forget to remove prebid from the configuration.
 // the timeout is the longest timeout in buckets if available, or arbitrary otherwise
@@ -104,17 +105,17 @@ const createdAdUnits = (
     .map(({ moliSlot, priceRule, filterSupportedSizes }) => {
       const floors: Pick<prebidjs.IAdUnit, 'floors'> | null = priceRule
         ? {
-            floors: {
-              currency: prebidConfig.config.currency?.adServerCurrency ?? 'EUR',
-              schema: {
-                delimiter: '|',
-                fields: ['mediaType']
-              },
-              values: {
-                '*': priceRule.floorprice
-              }
+          floors: {
+            currency: prebidConfig.config.currency?.adServerCurrency ?? 'EUR',
+            schema: {
+              delimiter: '|',
+              fields: ['mediaType']
+            },
+            values: {
+              '*': priceRule.floorprice
             }
           }
+        }
         : null;
       context.logger__.debug(
         'Prebid',
@@ -159,37 +160,37 @@ const createdAdUnits = (
           const videoDimensionsWH =
             videoSizes.length > 0 && !mediaTypeVideo?.w && !mediaTypeVideo?.h
               ? {
-                  w: videoSizes[0][0],
-                  h: videoSizes[0][1]
-                }
+                w: videoSizes[0][0],
+                h: videoSizes[0][1]
+              }
               : {};
 
           const backupVideoRenderer = prebidConfig.backupVideoRenderer;
           const video: prebidjs.IMediaTypes | undefined = mediaTypeVideo
             ? {
-                video: {
-                  ...mediaTypeVideo,
-                  playerSize: videoSizes.length === 0 ? undefined : videoSizes,
-                  ...videoDimensionsWH,
-                  // inject a backup video renderer if configured
-                  ...(backupVideoRenderer
-                    ? { renderer: prebidOutstreamRenderer(moliSlot.domId, backupVideoRenderer.url) }
-                    : {})
-                }
+              video: {
+                ...mediaTypeVideo,
+                playerSize: videoSizes.length === 0 ? undefined : videoSizes,
+                ...videoDimensionsWH,
+                // inject a backup video renderer if configured
+                ...(backupVideoRenderer
+                  ? { renderer: prebidOutstreamRenderer(moliSlot.domId, backupVideoRenderer.url) }
+                  : {})
               }
+            }
             : undefined;
 
           const banner =
             mediaTypeBanner && bannerSizes.length > 0
               ? {
-                  banner: { ...mediaTypeBanner, sizes: bannerSizes }
-                }
+                banner: { ...mediaTypeBanner, sizes: bannerSizes }
+              }
               : undefined;
 
           const native = mediaTypeNative
             ? {
-                native: { ...mediaTypeNative }
-              }
+              native: { ...mediaTypeNative }
+            }
             : undefined;
 
           const pubstack: prebidjs.IPubstackConfig = {
@@ -205,9 +206,9 @@ const createdAdUnits = (
           const storedRequestWithSolvedId: { id: string } | null =
             storedRequest && storedRequest.id
               ? {
-                  ...storedRequest,
-                  id: resolveAdUnitPath(storedRequest.id, context.adUnitPathVariables__)
-                }
+                ...storedRequest,
+                id: resolveAdUnitPath(storedRequest.id, context.adUnitPathVariables__)
+              }
               : null;
 
           return {
@@ -357,13 +358,14 @@ export const prebidConfigure = (
             });
           }
 
-          // TODO is is where additional HEM configuration would be added for id5 from the runtimeConfig.audience
           context.window__.pbjs.setConfig({
             ...prebidConfig.config,
             // global schain configuration
             ...{ schain: mkSupplyChainConfig([schainConfig.supplyChainStartNode]) },
             // for module priceFloors
-            ...{ floors: prebidConfig.config.floors || {} }
+            ...{ floors: prebidConfig.config.floors || {} },
+            // for
+            ...{ userSync: { userIds: [id5Config(context.runtimeConfig__)] } }
           });
 
           // set additional bidder configurations if provided
@@ -469,13 +471,13 @@ export const prebidRequestBids = (
 
         const requestObject: prebidjs.IRequestObj = prebidConfig.ephemeralAdUnits
           ? {
-              adUnits: createdAdUnits(context, prebidConfig, slotsToRefresh)
-            }
+            adUnits: createdAdUnits(context, prebidConfig, slotsToRefresh)
+          }
           : {
-              adUnitCodes: slotsToRefresh
-                .filter(isPrebidSlotDefinition)
-                .map(slot => slot.moliSlot.domId)
-            };
+            adUnitCodes: slotsToRefresh
+              .filter(isPrebidSlotDefinition)
+              .map(slot => slot.moliSlot.domId)
+          };
 
         // ad unit codes are required for setTargetingForGPTAsync
         const adUnitCodes =
