cb22a14026a44b05335d4045f99216ea56b0fb82
Emil Nadimanov <emil.nadimanov@gutefrage.net>
2025-11-06 15:35:02 +0100

    added a basic test for the analytics module

---
+++ ad-tag/source/ts/ads/modules/moli-analytics/index.test.ts
@@ -0,0 +1,76 @@
+import { expect, use } from 'chai';
+import * as Sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import { createDomAndWindow } from 'ad-tag/stubs/browserEnvSetup';
+import { MoliAnalytics } from 'ad-tag/ads/modules/moli-analytics/index';
+import { adPipelineContext } from 'ad-tag/stubs/adPipelineContextStubs';
+import { prebidjs } from 'ad-tag/types/prebidjs';
+import { createPbjsStub } from 'ad-tag/stubs/prebidjsStubs';
+
+use(sinonChai);
+
+describe('Moli Analytics Module', () => {
+  const sandbox = Sinon.createSandbox();
+  let { jsDomWindow } = createDomAndWindow();
+
+  const defaultContext = adPipelineContext(jsDomWindow);
+  const defaultUrl = 'https://example.com/analytics';
+  const defaultBatchSize = 10;
+
+  const createAndConfigureModule = (url: string, batchSize?: number) => {
+    const module = MoliAnalytics();
+    module.configure__({
+      moliAnalytics: {
+        enabled: true,
+        url: url,
+        batchSize: batchSize
+      }
+    });
+    return module;
+  };
+
+  beforeEach(() => {
+    jsDomWindow.pbjs = createPbjsStub();
+  });
+
+  afterEach(() => {
+    sandbox.reset();
+    sandbox.restore();
+  });
+
+  it('should load with the specified url', async () => {
+    const loadAnalyticsStub = sandbox.spy(jsDomWindow.pbjs, 'enableAnalytics');
+
+    const module = createAndConfigureModule(defaultUrl);
+    await module.initSteps__()[0](defaultContext);
+    expect(loadAnalyticsStub).to.have.been.calledOnce;
+    expect(loadAnalyticsStub.args[0][0][0].options).to.not.be.undefined;
+    const options = loadAnalyticsStub.args[0][0][0]
+      .options as prebidjs.analytics.IGenericAnalyticsAdapterOptions;
+    expect(options.url).to.equal(defaultUrl);
+  });
+
+  it('should load with the specified batch size', async () => {
+    const loadAnalyticsStub = sandbox.spy(jsDomWindow.pbjs, 'enableAnalytics');
+
+    const module = createAndConfigureModule(defaultUrl, defaultBatchSize);
+    await module.initSteps__()[0](defaultContext);
+    expect(loadAnalyticsStub).to.have.been.calledOnce;
+    expect(loadAnalyticsStub.args[0][0][0].options).to.not.be.undefined;
+    const options = loadAnalyticsStub.args[0][0][0]
+      .options as prebidjs.analytics.IGenericAnalyticsAdapterOptions;
+    expect(options.batchSize).to.equal(defaultBatchSize);
+  });
+
+  it('should not load if not configured', async () => {
+    const loadAnalyticsStub = sandbox.stub(jsDomWindow.pbjs, 'enableAnalytics');
+
+    const module = MoliAnalytics();
+    await module
+      .initSteps__()[0](defaultContext)
+      .catch(() => {
+        /* expected to fail */
+      });
+    expect(loadAnalyticsStub).to.not.have.been.called;
+  });
+});
